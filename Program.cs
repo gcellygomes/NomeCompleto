d852557b5af29e0de04e986ff7ee74740b72df3a
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-09-10 15:40:21 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//GicellyGomes
+
+Console.Write("Olá, usuario. \nQual o seu nome? ");
+
+
+string nome;
+nome = Console.ReadLine()!;
+Console.Write("Qual o seu sobrenome? ");
+
+string sobrenome;
+sobrenome = Console.ReadLine()!;
+
+Console.Write($"Olá, seja bem vindo, {nome} {sobrenome}.");
+Console.Write("Tenha um excelente dia");
